213b62935e9d8ef14886098cff5a314d3c1f5f8f
Morgan Griffiths <morgangriffiths@openai.com>
2024-01-02 00:37:04 -0800

    add entrypoint to run jedi lang server and call lsp tool.

---
+++ main.py
@@ -0,0 +1,48 @@
+import asyncio
+import os
+import sys
+
+
+import click
+from code_context.lsp_client import call_lsp
+from code_context.jedi_client import run_jedi
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("file_and_function", required=True)
+@click.argument("depth", default=1, type=int)
+def lsp(file_and_function, depth):
+    """
+    Run the LSP client on a given file and optional function.
+    Usage: lsp <file_name>::<function_name> <depth>
+    """
+    if "::" in file_and_function:
+        file_name, function_name = file_and_function.split("::")
+    else:
+        file_name = file_and_function
+        function_name = None
+
+    asyncio.run(call_lsp(file_name, function_name, depth))
+
+
+@cli.command()
+@click.argument(
+    "root_dir",
+    type=click.Path(exists=True),
+)
+def start_jedi(root_dir):
+    """
+    Start the Jedi client.
+    """
+
+    root_dir = sys.argv[1] if len(sys.argv) > 1 else None
+    asyncio.run(run_jedi(root_dir))
+
+
+if __name__ == "__main__":
+    cli()
